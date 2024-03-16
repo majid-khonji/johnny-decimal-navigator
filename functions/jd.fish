d7bf7a4ec391319c6cc80e911460e549c642b7be
Majid Khonji <majid.khonji@gmail.com>
2024-03-16 06:17:57 +0400

    Now the directory can be inserted into the commandline, so it can have other uses besides cd

--- functions/jd.fish
+++ functions/jd.fish
@@ -1,4 +1,7 @@
 function jd
-    cd (find ~/Dropbox/ -maxdepth 3 -type d -iregex '.*/[0-9][0-9]\(\.[0-9][0-9]?\)?.*' -not -path "$HOME/Dropbox/00-notes*" | fzf)
+    find ~/Dropbox/ -maxdepth 3 -type d -iregex '.*/[0-9][0-9]\(\.[0-9][0-9]?\)?.*' -not -path "$HOME/Dropbox/00-notes*" | fzf | read -l result 
+    if test -n "$result"
+        commandline -i -- \'$result\'
+    end
 end
 
